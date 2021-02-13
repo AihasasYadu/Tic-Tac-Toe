0fcffe57c023558a39a6a32b20e4dd7463269ff3
Aihasas Yadu <yadu.aihasas@gmail.com>
2021-02-13 21:45:45 +0530

    Additions: AssetBundle Creator Script Asset Bundle Loading Script Logo Prefab

---
+++ Assets/Scripts/Editor/AssetBundleCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundleCreator : MonoBehaviour
+{
+    [MenuItem("Assets/Build Assets")]
+
+    public static void BuildAssetBundle()
+    {
+        string assetBundleDirectory = "Assets/StreamingAssets";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssetBundle : MonoBehaviour
+{
+    [SerializeField] private string url = "";
+    [SerializeField] private string assetName = "";
+    void Start()
+    {
+        WWW web = new WWW(url);
+        StartCoroutine(WebRequest(web));
+    }
+
+    private IEnumerator WebRequest(WWW w3)
+    {
+        yield return w3;
+        while (!w3.isDone)
+        {
+            yield return null;
+        }
+
+        AssetBundle bundle = w3.assetBundle;
+        if(w3.error == null)
+        {
+            GameObject gameObj = (GameObject)bundle.LoadAsset(assetName);
+            Instantiate(gameObj);
+        }
+        else
+        {
+            Debug.Log(w3.error);
+        }
+    }
+}
